7fbd03b2eba176cebad907c4b2a81ab7614d1e16
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-09-28 21:09:06 +0530

    Header nav for portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+import { Outfit, Ovo } from 'next/font/google';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+    "./pages/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,mdx}",
+    "./app/**/*.{js,jsx,ts,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+        colors: {
+            lightHover:'#fcf4ff',
+            darkHover: '#2a004a',
+            darkTheme: '#11001F'
+        },
+        fontFamily: {
+            Outfit: ["Outfit", "sans-serif"],
+            Ovo: ["Ovo", "serif"]
+        }
+    },
+  },
+  plugins: [],
+}
+
